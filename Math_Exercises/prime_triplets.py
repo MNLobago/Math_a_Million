3dcffb196ffebab62430768b658c3dbec9b68fcb
Moan Nyatuka Lobago <lobago2407@gmail.com>
2024-05-25 18:23:19 +0300

    Fixed a BUG

--- Math_Exercises/prime_triplets.py
+++ Math_Exercises/prime_triplets.py
@@ -1,24 +1,30 @@
 def is_prime(n):
     if n <= 1:
         return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
             return False
+        i += 6
     return True
 
-def count_prime_triplets(arr):
+def count_triplets(arr):
     count = 0
     n = len(arr)
     for i in range(n):
         for j in range(i + 1, n):
             for k in range(j + 1, n):
-                if is_prime(arr[i] + arr[j] + arr[k]):
+                if is_prime(arr[i] + arr[j]) and is_prime(arr[j] + arr[k]) and is_prime(arr[k] + arr[i]):
                     count += 1
     return count
 
 if __name__ == "__main__":
-    t = int(input())
+    t = int(input("Enter the number of test cases: "))
     for _ in range(t):
-        n = int(input())
-        arr = list(map(int, input().split()))
-        print(count_prime_triplets(arr))
+        n = int(input("Enter the length of the array: "))
+        arr = list(map(int, input("Enter the space-separated elements of the array: ").split()))
+        print(count_triplets(arr))
